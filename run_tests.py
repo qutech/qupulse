ee53a385cf58b19c345a0231301ccee38c7363bb
j340m3 <j340m3@hotmail.com>
2015-06-21 11:47:04 +0200

    Introduced Typechecking and their unittest

--- run_tests.py
+++ run_tests.py
@@ -9,4 +9,4 @@ srcPath = basePath + '/src/'
 sys.path.insert(0, srcPath)
 
 suite = unittest.TestLoader().discover('.', pattern='*Test*.py')
-unittest.TextTestRunner().run(suite)
+unittest.TextTestRunner(verbosity=2).run(suite)
