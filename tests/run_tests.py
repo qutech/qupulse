c33fa94c2a0ce1a34cad21d87d21bea3b02d5bff
Lukas Prediger <lukas.prediger@rwth-aachen.de>
2015-07-14 13:56:37 +0200

    Removed run_tests.py: obsolete.

--- tests/run_tests.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-import unittest
-
-#basePath = os.path.dirname(os.path.abspath(__file__))
-#sys.path.insert(0, basePath)
-
-suite = unittest.TestLoader().discover('.', pattern='*Test*.py')
-unittest.TextTestRunner(verbosity=2).run(suite)
