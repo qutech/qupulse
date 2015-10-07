5bfc1487cf312ef740ca25baf31ca331760c8d09
Eugen Kammerloher <eugen.kammerloher@gmail.com>
2015-10-07 09:50:14 +0200

    Restrict setup to python version 3.3 and above

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
 import sys
 
+if sys.version_info < (3, 3):
+    sys.stderr.write('ERROR: You need Python 3.3 or later '
+                     'to install the qctoolkit package.\n')
+    exit(1)
+
 if sys.version_info < (3, 5):
     requires_typing = ['typing==3.5.0']
 else:
