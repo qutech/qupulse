2ae6f1d2c94daeaaf026821730bbf7fa5a7d3213
Lukas Prediger <lukas.prediger@rwth-aachen.de>
2015-10-15 15:05:48 +0200

    qcmatlab package now no longer automatically connects to MATLAB upon import. Use qcmatlab.manager.Manager.getEngine() instead.

--- src/qcmatlab/__init__.py
+++ src/qcmatlab/__init__.py
@@ -1,8 +1,1 @@
-# intended use:
-# from qctoolkit.qcmatlab import engine
-# engine.workspace['max_variable'] = 42
-# engine.workspace['foo'] = 'bar'
-from .manager import Manager
-
-engine = Manager.getEngine()
-connect = Manager.connectTo
+__all__ = ['Manager']
