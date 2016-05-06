60857246d6e1e1e162332223a91731c276219d32
Lukas Prediger <lukas.prediger@rwth-aachen.de>
2016-05-06 13:27:23 +0200

    Docstrings for Comparable.

--- qctoolkit/comparable.py
+++ qctoolkit/comparable.py
@@ -6,16 +6,23 @@ __all__ = ["Comparable"]
 
 
 class Comparable(metaclass=ABCMeta):
+    """An object that can be queried for equality with other Comparable object."""
 
     @abstractproperty
     def _compare_key(self) -> Any:
-        """Return a unique key used in comparison and hashing operations."""
+        """Return a unique key used in comparison and hashing operations.
+
+        The key must describe the essential properties of the object. Two objects are equal iff their keys are identical.
+        """
 
     def __hash__(self) -> int:
+        """Returns a hash value of the comparable object."""
         return hash(self._compare_key)
 
     def __eq__(self, other: Any) -> bool:
+        """True, if other is equal to self."""
         return isinstance(other, self.__class__) and self._compare_key == other._compare_key
 
     def __ne__(self, other: Any) -> bool:
+        """True, if other is not equal to self."""
         return not self == other
