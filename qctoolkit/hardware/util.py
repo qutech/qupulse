eee5d6836736f79eb70cbab79ab21feb24497815
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2017-03-09 18:10:37 +0100

    Replace pytabor make_combined_wave with more efficient and adapted variant TESTED!

--- qctoolkit/hardware/util.py
+++ qctoolkit/hardware/util.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 import numpy as np
 
 __all__ = ['voltage_to_uint16']
@@ -24,3 +26,43 @@ def voltage_to_uint16(voltage: np.ndarray, output_amplitude: float, output_offse
     non_dc_voltage *= (2**resolution - 1) / (2*output_amplitude)
     np.rint(non_dc_voltage, out=non_dc_voltage)
     return non_dc_voltage.astype(np.uint16)
+
+
+def make_combined_wave(segments: List['TaborSegment'], destination_array=None, fill_value=None) -> np.ndarray:
+    quantum = 16
+    if len(segments) == 0:
+        return np.zeros(0, dtype=np.uint16)
+    segment_lengths = np.fromiter((segment.num_points for segment in segments), count=len(segments), dtype=int)
+    if np.any(segment_lengths % quantum != 0):
+        raise ValueError('Segment is not a multiple of 16')
+    n_quanta = np.sum(segment_lengths) // quantum + len(segments) - 1
+
+    if destination_array is not None:
+        if len(destination_array) != 2*n_quanta*quantum:
+            raise ValueError('Destination array has an invalid length')
+        destination_array = destination_array.reshape((2*n_quanta, quantum))
+    else:
+        destination_array = np.empty((2*n_quanta, quantum), dtype=np.uint16)
+    if fill_value:
+        destination_array[:] = fill_value
+
+    segment_quanta = 2 * segment_lengths[0] // quantum
+    if segments[0][1] is not None:
+        destination_array[0:segment_quanta:2].flat = segments[0][1]
+    if segments[0][0] is not None:
+        destination_array[1:segment_quanta:2].flat = segments[0][0]
+
+    current_quantum = segment_quanta
+    for (chan_a, chan_b), segment_length in zip(segments[1:], segment_lengths[1:]):
+        segment_quanta = 2 * (segment_length // quantum + 1)
+        segment_destination = destination_array[current_quantum:current_quantum+segment_quanta, :]
+
+        if chan_b is not None:
+            segment_destination[0, :] = chan_b[0]
+            segment_destination[2::2, :].flat = chan_b
+        if chan_a is not None:
+            segment_destination[1, :] = chan_a[0]
+            segment_destination[3::2, :].flat = chan_a
+        current_quantum += segment_quanta
+    return destination_array.ravel()
+
--- tests/hardware/util_tests.py
+++ tests/hardware/util_tests.py
@@ -1,7 +1,11 @@
 import unittest
+import itertools
+
+import pytabor
 import numpy as np
 
-from qctoolkit.hardware.util import voltage_to_uint16
+from qctoolkit.hardware.awgs.tabor import TaborSegment
+from qctoolkit.hardware.util import voltage_to_uint16, make_combined_wave
 
 
 class VoltageToBinaryTests(unittest.TestCase):
@@ -22,3 +26,55 @@ class VoltageToBinaryTests(unittest.TestCase):
         received_data = voltage_to_uint16(linspace_voltage, 0.5, 0.5, 7)
 
         self.assertTrue(np.all(expected_data == received_data))
+
+
+class TaborMakeCombinedTest(unittest.TestCase):
+
+    def exec_general(self, data_1, data_2):
+        tabor_segments = [TaborSegment(d1, d2) for d1, d2 in zip(data_1, data_2)]
+        expected_length = (sum(segment.num_points for segment in tabor_segments) + 16 * (len(tabor_segments) - 1)) * 2
+
+        offset = 0
+        pyte_result = 15000*np.ones(expected_length, dtype=np.uint16)
+        for i, segment in enumerate(tabor_segments):
+            offset = pytabor.make_combined_wave(segment[0], segment[1],
+                                                dest_array=pyte_result, dest_array_offset=offset,
+                                                add_idle_pts=i > 0)
+        self.assertEqual(expected_length, offset)
+
+        result = make_combined_wave(tabor_segments, fill_value=15000)
+        np.testing.assert_equal(pyte_result, result)
+
+        dest_array = 15000*np.ones(expected_length, dtype=np.uint16)
+        result = make_combined_wave(tabor_segments, destination_array=dest_array)
+        np.testing.assert_equal(pyte_result, result)
+        # test that the destination array data is not copied
+        self.assertEqual(dest_array.__array_interface__['data'],
+                         result.__array_interface__['data'])
+
+        with self.assertRaises(ValueError):
+            make_combined_wave(tabor_segments, destination_array=np.ones(16))
+
+
+    def test_make_comb_both(self):
+        gen = itertools.count()
+        data_1 = [np.fromiter(gen, count=32, dtype=np.uint16),
+                  np.fromiter(gen, count=16, dtype=np.uint16),
+                  np.fromiter(gen, count=192, dtype=np.uint16)]
+
+        data_2 = [np.fromiter(gen, count=32, dtype=np.uint16),
+                  np.fromiter(gen, count=16, dtype=np.uint16),
+                  np.fromiter(gen, count=192, dtype=np.uint16)]
+
+        self.exec_general(data_1, data_2)
+
+    def test_make_single_chan(self):
+        gen = itertools.count()
+        data_1 = [np.fromiter(gen, count=32, dtype=np.uint16),
+                  np.fromiter(gen, count=16, dtype=np.uint16),
+                  np.fromiter(gen, count=192, dtype=np.uint16)]
+
+        data_2 = [None]*len(data_1)
+        self.exec_general(data_1, data_2)
+        self.exec_general(data_2, data_1)
+
