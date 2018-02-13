5b917648509be46066db5042f1bf31dabad8f58b
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2018-02-13 16:22:47 +0100

    Remove rint call

--- qctoolkit/hardware/util.py
+++ qctoolkit/hardware/util.py
@@ -24,7 +24,6 @@ def voltage_to_uint16(voltage: np.ndarray, output_amplitude: float, output_offse
                                                   output_amplitude=output_amplitude))
     non_dc_voltage += output_amplitude
     non_dc_voltage *= (2**resolution - 1) / (2*output_amplitude)
-    np.rint(non_dc_voltage, out=non_dc_voltage)
     return non_dc_voltage.astype(np.uint16)
 
 
