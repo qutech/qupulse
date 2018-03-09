ec933ea720f96011b7684e4b3b980b12c2dbfe25
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2018-03-09 12:33:08 +0100

    FIX import bug by removing dead code

--- qctoolkit/pulses/multi_channel_pulse_template.py
+++ qctoolkit/pulses/multi_channel_pulse_template.py
@@ -123,10 +123,6 @@ class MultiChannelWaveform(Waveform):
                       output_array: Union[numpy.ndarray, None]=None) -> numpy.ndarray:
         return self[channel].unsafe_sample(channel, sample_times, output_array)
 
-    def get_measurement_windows(self) -> Iterable[MeasurementWindow]:
-        return itertools.chain.from_iterable(sub_waveform.get_measurement_windows()
-                                             for sub_waveform in self._sub_waveforms)
-
     def unsafe_get_subset_for_channels(self, channels: Set[ChannelID]) -> 'Waveform':
         relevant_sub_waveforms = tuple(swf for swf in self._sub_waveforms if swf.defined_channels & channels)
         if len(relevant_sub_waveforms) == 1:
