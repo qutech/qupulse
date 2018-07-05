768b1439914b8bfc656ce63f859bc9561f2e5af9
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2018-07-05 10:30:30 +0200

    Tektronix WIP

--- qupulse/utils/__init__.py
+++ qupulse/utils/__init__.py
@@ -1,8 +1,9 @@
-from typing import Union
+from typing import Union, Iterable, Any, Tuple
+import itertools
 
 import numpy
 
-__all__ = ["checked_int_cast", "is_integer"]
+__all__ = ["checked_int_cast", "is_integer", "pairwise"]
 
 
 def checked_int_cast(x: Union[float, int, numpy.ndarray], epsilon: float=1e-6) -> int:
@@ -19,3 +20,20 @@ def checked_int_cast(x: Union[float, int, numpy.ndarray], epsilon: float=1e-6) -
 
 def is_integer(x: Union[float, int], epsilon: float=1e-6) -> bool:
     return abs(x - int(round(x))) < epsilon
+
+
+def pairwise(iterable: Iterable[Any],
+             zip_function=itertools.zip_longest, **kwargs) -> Iterable[Tuple[Any, Any]]:
+    """s -> (s0,s1), (s1,s2), (s2, s3), ...
+
+    Args:
+        iterable: Iterable to iterate over pairwise
+        zip_function: Either zip or itertools.zip_longest(default)
+        **kwargs: Gets passed to zip_function
+
+    Returns:
+        An iterable that yield neighbouring elements
+    """
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip_function(a, b, **kwargs)
