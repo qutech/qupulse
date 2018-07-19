39a4da43ed24742db60275f9d89ae4fcf9a4702f
Lukas Prediger <lukas.prediger@rwth-aachen.de>
2018-07-19 14:06:13 +0200

    Fix: Ensured that MappingPulseTemplate is also know under alias module qctoolkit.pulses.pulse_template_parameter_mapping to Serializable for backward compatible deserialization.

--- qctoolkit/pulses/pulse_template_parameter_mapping.py
+++ qctoolkit/pulses/pulse_template_parameter_mapping.py
@@ -6,3 +6,6 @@ import warnings
 warnings.warn("MappingPulseTemplate was moved from qctoolkit.pulses.pulse_template_parameter_mapping to "
               "qctoolkit.pulses.mapping_pulse_template. Please consider fixing your stored pulse templates by loading "
               "and storing them anew.", DeprecationWarning)
+
+from qctoolkit.serialization import SerializableMeta
+SerializableMeta.deserialization_callbacks["qctoolkit.pulses.pulse_template_parameter_mapping.MappingPulseTemplate"] = SerializableMeta.deserialization_callbacks[MappingPulseTemplate.get_type_identifier()]
