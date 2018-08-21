b048bf62304931828af5c4a27942202831c00455
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2018-08-21 14:12:26 +0200

    Replace custom class with builtin class

--- qctoolkit/utils/types.py
+++ qctoolkit/utils/types.py
@@ -10,7 +10,7 @@ from collections.abc import Mapping as ABCMapping
 import numpy
 
 __all__ = ["MeasurementWindow", "ChannelID", "HashableNumpyArray", "TimeType", "time_from_float", "DocStringABCMeta",
-           "OrderedSet", "ConstantDictView", "SingletonABCMeta"]
+           "SingletonABCMeta"]
 
 MeasurementWindow = typing.Tuple[str, numbers.Real, numbers.Real]
 ChannelID = typing.Union[str, int]
@@ -72,55 +72,3 @@ class HashableNumpyArray(numpy.ndarray):
     """
     def __hash__(self):
         return hash(self.tobytes())
-
-
-class ConstantDictView(ABCMapping):
-    """Immutable view on a dict"""
-    __slots__ = ("__getitem__", "__contains__", "keys", "values", "items", "__iter__", "__len__", "get")
-
-    def __init__(self, dict_obj):
-        for slot in self.__slots__:
-            setattr(self, slot, getattr(dict_obj, slot))
-
-
-class OrderedSet(collections.OrderedDict, collections.MutableSet):
-    def update(self, *args, **kwargs):
-        if kwargs:
-            raise TypeError("update() takes no keyword arguments")
-
-        update = super(collections.OrderedDict, self).update
-        for s in args:
-            update(itertools.zip_longest(s, ()))
-
-    def add(self, elem):
-        self[elem] = None
-
-    def discard(self, elem):
-        self.pop(elem, None)
-
-    def remove(self, element):
-        self.pop(element)
-
-    def __le__(self, other):
-        return self.keys() <= other.keys()
-
-    def __lt__(self, other):
-        return self.keys() < other.keys()
-
-    def __ge__(self, other):
-        return self.keys() >= other.keys()
-
-    def __gt__(self, other):
-        return self.keys() > other.keys()
-
-    def __eq__(self, other):
-        return self.keys() == other.keys()
-
-    def __ne__(self, other):
-        return self.keys() != other.keys()
-
-    def __repr__(self):
-        return 'OrderedSet([%s])' % (', '.join(map(repr, self.keys())))
-
-    def __str__(self):
-        return '{%s}' % (', '.join(map(repr, self.keys())))
