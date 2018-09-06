1640b7cbcd23ddd34a89ba14fbafe50f424f40a7
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2018-09-06 15:37:55 +0200

    Import all qupulse subpackages and modules when importing qctoolkit

--- qctoolkit/__init__.py
+++ qctoolkit/__init__.py
@@ -1,4 +1,22 @@
 import sys
+import re
+import pkgutil
 import importlib
 
-sys.modules[__name__] = importlib.import_module('qupulse')
+qupulse = importlib.import_module('qupulse')
+sys.modules[__name__] = qupulse
+
+aliased = {}
+
+for _, name, ispkg in pkgutil.walk_packages(qupulse.__path__, 'qupulse.'):
+    alias = re.sub('^qupulse.', '%s.' % __name__, name)
+
+    try:
+        imported = importlib.import_module(name)
+    except ImportError:
+        print('Could not import %s. The alias %s was NOT created.' % (name, alias))
+        continue
+    sys.modules[alias] = imported
+    aliased[alias] = name
+
+print('Created module aliases:', aliased)
