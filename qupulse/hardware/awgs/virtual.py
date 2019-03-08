68d37196fe4fdac0bce2bbf639110f708195c872
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2019-03-08 12:55:50 +0100

    Remove fixed sample rate callbacks for now

--- qupulse/hardware/awgs/virtual.py
+++ qupulse/hardware/awgs/virtual.py
@@ -16,48 +16,6 @@ SamplingCallback = Callable[[np.ndarray], np.ndarray]
 SamplingCallback.__doc__ = """Maps an array ov times to an array of voltages. The time array has to be ordered"""
 
 
-def _feed_into_callable_fixed_sample_rate(program: Loop, callback, channels, dt, voltage_transformations):
-    """Maybe there is already a function somewhere for this?"""
-    binary_waveforms = {}
-    reverse_lookup = {}
-    time_array = np.arange(1024, dtype=float) * float(dt)
-    for p in program.get_depth_first_iterator():
-        if p.waveform and p.waveform not in binary_waveforms:
-            # this waveform is not sampled yet
-            n_samples = p.waveform.duration / dt
-
-            if time_array.size < n_samples:
-                # we need to extend the time array
-                time_array = np.arange(n_samples, dtype=float) * float(dt)
-
-            sample_times = time_array[:n_samples]
-            sampled = np.nan((len(channels), n_samples))
-            for idx, (channel, voltage_transformation) in enumerate(zip(channels, voltage_transformations)):
-                p.waveform.get_sampled(channel, sample_times, output_array=sampled[idx, :])
-                sampled[idx, :] = voltage_transformation(sampled[idx, :])
-
-            result_hash = hash(sampled.tobytes())
-            if result_hash in reverse_lookup:
-                # save memory for binary equivalent waveforms
-                np.testing.assert_array_equal(sampled, reverse_lookup[result_hash])
-                binary_waveforms[p.waveform] = reverse_lookup[result_hash]
-            else:
-                binary_waveforms[p.waveform] = sampled
-                reverse_lookup[result_hash] = sampled
-
-    stack = [program]
-    while stack:
-        current = stack.pop()
-        if current.waveform:
-            sampled = binary_waveforms[current.waveform]
-            for _ in range(current.repetition_count):
-                callback(sampled)
-
-        else:
-            for _ in range(current.repetition_count):
-                stack.extend(reversed(current))
-
-
 def _create_sampling_callbacks(program: Loop, channels, voltage_transformations):
     waveform = to_waveform(program)
 
@@ -90,7 +48,6 @@ class VirtualAWG(AWG):
         self._current_program = None
         self._channels = tuple(range(channels))
 
-        self._fixed_sample_rate_callbacks = {}
         self._function_handle_callback = None
 
     @property
@@ -130,12 +87,6 @@ class VirtualAWG(AWG):
     def sample_rate(self) -> float:
         return float('nan')
 
-    def set_fixed_sample_rate_callback(self, name: str, callback: Callable[[np.ndarray], None], sample_rate: TimeType):
-        if callback is None:
-            self._fixed_sample_rate_callbacks.pop(name, None)
-        else:
-            self._fixed_sample_rate_callbacks[name] = (callback, sample_rate)
-
     def set_function_handle_callback(self,
                                      callback: Optional[Callable[[float, SamplingCallback, ...], None]]):
         """When run current program is called the given callback is called with the first positional argument being the
@@ -144,16 +95,6 @@ class VirtualAWG(AWG):
 
     def run_current_program(self):
         (program, channels, voltage_transformations) = self._programs[self._current_program]
-
-        for callback, sample_rate in self._fixed_sample_rate_callbacks.values():
-            dt = 1/sample_rate
-            c_program = program.copy()
-
-            # assert all waveforms have a length that is a multiple of the time per sample
-            make_compatible(c_program, 0, dt, sample_rate)
-
-            _feed_into_callable_fixed_sample_rate(c_program, callback=callback, channels=channels, dt=dt,
-                                                  voltage_transformations=voltage_transformations)
 
         if self._function_handle_callback is not None:
             duration, *sample_callbacks = _create_sampling_callbacks(program, channels, voltage_transformations)
