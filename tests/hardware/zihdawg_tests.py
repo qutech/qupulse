c18edea2a37cadb14e32257000aa35479ed11a16
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2019-06-11 20:17:20 +0200

    Add test stubs for zihdawg

---
+++ tests/hardware/zihdawg_tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from qupulse.hardware.awgs.zihdawg import HDAWGChannelPair, HDAWGWaveManager
+
+
+class HDAWGRepresentation(unittest.TestCase):
+    def test_init(self):
+        raise NotImplementedError()
+
+
+class HDAWGChannelPairTests(unittest.TestCase):
+    def test_init(self):
+        raise NotImplementedError()
+
+
+class HDAWGWaveManagerTests(unittest.TestCase):
+    def test_init(self):
+        raise NotImplementedError()
