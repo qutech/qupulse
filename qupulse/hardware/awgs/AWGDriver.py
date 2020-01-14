cb605defe803c0372a8bc5356a8610ac4a4f4ba3
Papajewski <b.papajewski@fz-juelich.de>
2020-01-14 10:22:25 +0100

    Erste Verbesserungen eingebaut

--- qupulse/hardware/awgs/AWGDriver.py
+++ qupulse/hardware/awgs/AWGDriver.py
@@ -1,46 +1,76 @@
 from abc import ABC, abstractmethod
+from qupulse._program._loop import Loop
+from typing import TypeVar, Generic, List
 
+
+# TODO: Abstrakte Getter und Setter
 
 class BaseFeature(ABC):
     def __init__(self):
-        self._functionList = []
+        super().__init__()
+
+        self._function_list = {}
+
         directory = dir(self)
-        tmp_list = []
         i = 0
         for attr in directory:
             if callable(getattr(self, attr)) and attr[0] != "_":
-                tmp_list.append(attr)
+                if not (attr in self._function_list):
+                    self._function_list[attr] = getattr(self, attr)
+                    # self._functionList.append(attr)
+
+    # TODO: es heisst function_list aber ist ein Dictionary
 
     @property
     def function_list(self):
-        return tuple(self._functionList)
+        return tuple(self._function_list)
 
 
-class FeatureList(ABC):
+FeatureType = TypeVar(BaseFeature)
+
+
+class FeatureList(Generic[FeatureType], ABC):
     def __init__(self):
-        self._featureList = []
+        super().__init__()
 
-    def add_feature(self, feature: BaseFeature):
+        self._feature_list = {}
+
+    @property
+    def feature_list(self):
+        return self._feature_list
+
+    def add_feature(self, feature: FeatureType):
         for function in feature.function_list:
             setattr(self, function, getattr(feature, function))
 
-        self._featureList.append(feature)
+        # self._feature_list.append(feature)
+        self._feature_list[type(feature).__name__] = feature
+
+    @abstractmethod
+    def _send_cmd(self, cmd: str):
+        pass
+
+    @abstractmethod
+    def _send_query(self, cmd: str) -> str:
+        pass
 
 
-class AWGDeviceFeature(ABC, BaseFeature):
+class AWGDeviceFeature(BaseFeature, ABC):
     pass
 
 
-class AWGChannelFeature(ABC, BaseFeature):
+class AWGChannelFeature(BaseFeature, ABC):
     pass
 
 
-class AWGChannelTupleFeature(ABC, BaseFeature):
+class AWGChannelTupleFeature(BaseFeature, ABC):
     pass
 
 
 class Program(ABC):
-    def __init__(self, name: str, program: Loop):
+    def __init__(self, name: str, program: "Loop"):
+        super().__init__()
+
         self._name = name
         self._program = program
         self._channel_ids = []
@@ -81,76 +111,73 @@ class AWGProgramManager(ABC):
         pass
 
 
-class AWGDevice(ABC, FeatureList):
+class AWGDevice(FeatureList[AWGDeviceFeature], ABC):
     def __init__(self):
-        self._channels = []
-        self._channel_groups = []
+        super().__init__()
 
-    def add_feature(self, feature: AWGDeviceFeature):
-        super().add_feature(feature)
+        self._channels = List["AWGChannel"]  # TODO: "AWGChannel"
+        self._channel_groups = List[AWGChannelTuple]
 
     @abstractmethod
     def initialize(self):
         pass
 
     @abstractmethod
-    def synchronoize_channels(self, group: int):
-        pass
-
-    @abstractmethod
-    def _send_cmd(self, cmd: str):
-        pass
-
-    @abstractmethod
-    def _send_querry(self, cmd: str) -> str:
+    def cleanup(self):
         pass
 
     @property
     def channels(self):
         return self._channels
 
+    # TODO: Ueberpruefung in " "
+    #@channels.setter
+    #@abstractmethod
+    #def channels(self, channels: List["AWGChannel"]):
+    #    pass
+
     @property
     def channel_group(self):
         return self._channel_groups
 
 
-class AWGChannelTuple(ABC, FeatureList):
-    def __init__(self, channel_tuple_id: int, device: AWGDevice, channels, sample_rate: float,
-                 programs: AWGProgramManager):
-        self._channel_tuptle_id = channel_tuple_id
+class AWGChannelTuple(FeatureList[AWGChannelTupleFeature], ABC):
+    def __init__(self, channel_tuple_id: int, device: AWGDevice, channels):
+        super().__init__()
+
+        self._channel_tuple_id = channel_tuple_id
         self._device = device
         self._channels = channels
-        self._sample_rate = sample_rate
-        self._programs = programs
 
-    def add_feature(self, feature: AWGChannelTupleFeature):
-        super().add_feature(feature)
+    #@property
+    #@abstractmethod
+    #def sample_rate(self):
+    #    pass
 
-    @abstractmethod
-    def _send_cmd(self, cmd: str):
-        pass
+    #@sample_rate.setter
+    #@abstractmethod
+    #def channel_tuple(self, sample_rate: float):
+    #   pass
 
-    @abstractmethod
-    def _send_querry(self, cmd: str) -> str:
-        pass
+    @property
+    def channel_tuple_id(self):
+        return self._channel_tuple_id
+
+    @property
+    def device(self):
+        return self._device
+
+    @property
+    def channels(self):
+        return self._channels
 
 
-class AWGChannel(ABC, FeatureList):
-    def __init__(self, channel_id: int, device: AWGDevice, channel_tupel: AWGChannelTuple):
+class AWGChannel(FeatureList[AWGChannelFeature], ABC):
+    def __init__(self, channel_id: int, device: AWGDevice):
+        super().__init__()
         self._channel_id = channel_id
         self._device = device
-        self._channel_tupel = channel_tupel
-
-    def add_feature(self, feature: AWGChannelFeature):
-        super().add_feature(feature)
-
-    @abstractmethod
-    def _send_cmd(self, cmd: str):
-        pass
-
-    @abstractmethod
-    def _send_querry(self, cmd: str) -> str:
-        pass
+        self._channel_tuple = None
 
     @property
     def channel_id(self):
@@ -161,5 +188,11 @@ class AWGChannel(ABC, FeatureList):
         return self._device
 
     @property
-    def channel_tupel(self):
+    def channel_tuple(self):
         return self._channel_tupel
+
+    # TODO: @channel_tuple.setter da hat kann es keinen _ davor haben. -> Namensgebungs?
+
+    @channel_tuple.setter
+    def channel_tuple(self, channel_tuple: AWGChannelTuple):
+        self._channel_tuple = channel_tuple
