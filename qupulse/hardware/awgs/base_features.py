3938c5819b78fe5d16e5cb695bb72dfafd783a9b
Papajewski <b.papajewski@fz-juelich.de>
2020-01-27 15:49:55 +0100

    Change of the feature-logic

--- qupulse/hardware/awgs/base_features.py
+++ qupulse/hardware/awgs/base_features.py
@@ -1,64 +1,37 @@
+import typing
+from typing import TypeVar, Generic, Dict, Callable
 import warnings
 from abc import ABC
 from copy import copy
-from typing import TypeVar, Generic, Dict, Callable
 
 
 class BaseFeature(ABC):
     """
     Base class for features of `FeatureAble`s.
-
-    Features are classes containing functions which are bound dynamically to the target instance of type `FeatureAble`.
-    This ensures that all targets for the same feature are using the same signature for the feature's functions. All
-    public callables of a specific feature will be added to the function dictionary. Those functions (in the `functions`
-    dictionary) will be automatically added to the specific `FeatureAble` that calls `FeatureAble.add_feature`.
     """
-
-    def __init__(self):
-        super().__init__()
-
-        self._functions = self._read_functions()
-
-    def _read_functions(self) -> Dict[str, Callable]:
-        """
-        Reads the functions of a feature and returns them as a dictionary
-
-        Return:
-            Returns dictionary with all functions of the feature
-        """
-        directory = dir(self)
-        function_list: Dict[str, Callable] = {}
-        for attr in directory:
-            if callable(getattr(type(self), attr)) and attr[0] != "_":
-                if not (attr in function_list):
-                    function_list[attr] = getattr(self, attr)
-        return function_list
-
-    @property
-    def functions(self) -> Dict[str, Callable]:
-        """Returns a copy of the dictionary with all public functions of the feature"""
-        return copy(self._functions)
+    pass
 
 
 FeatureType = TypeVar("FeatureType", bound=BaseFeature)
 
 
 class FeatureAble(Generic[FeatureType], ABC):
-    """Base class for all classes that are able to add features"""
+    """
+    Base class for all classes that are able to add features. The features are saved in a dictonary and the methods
+    can be called with __getitem__.
+    """
 
     def __init__(self):
         super().__init__()
 
         self._features = {}
 
-    @property
-    def features(self) -> Dict[str, Callable]:
-        """Returns the dictionary with all features of a FeatureAble"""
-        return copy(self._features)
+    def __getitem__(self, feature_type: typing.Type[FeatureType]) -> FeatureType:
+        return self._features[feature_type]
 
     def add_feature(self, feature: FeatureType) -> None:
         """
-        The method adds all functions of feature to a dictionary with all functions
+        The method adds the feature to a Dictionary with all features
 
         Args:
              feature: A certain feature which functions should be added to the dictionary _features
@@ -66,10 +39,9 @@ class FeatureAble(Generic[FeatureType], ABC):
         if not isinstance(feature, BaseFeature):
             raise TypeError("Invalid type for feature")
 
-        for function in feature.functions:
-            if not hasattr(self, function):
-                setattr(self, function, getattr(feature, function))
-            else:
-                warnings.warn(f"Omitting function \"{function}\": Another attribute with this name already exists.")
+        self._features[type(feature)] = feature
 
-        self._features[type(feature).__name__] = feature
+    @property
+    def features(self) -> Dict[FeatureType, Callable]:
+        """Returns the dictionary with all features of a FeatureAble"""
+        return copy(self._features)
