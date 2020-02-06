a1c5f3ae0372893793c94b980bfff41c343e948d
Lukas.Lankes <l.lankes@fz-juelich.de>
2020-02-06 07:47:39 +0100

    Replaced imports of awgs.base with awgs.old_base

--- qupulse/_program/seqc.py
+++ qupulse/_program/seqc.py
@@ -33,8 +33,9 @@ from qupulse.utils.types import ChannelID, TimeType
 from qupulse.utils import replace_multiple
 from qupulse._program.waveforms import Waveform
 from qupulse._program._loop import Loop
+
 from qupulse._program.volatile import VolatileRepetitionCount, VolatileProperty
-from qupulse.hardware.awgs.base import ProgramEntry
+from qupulse.hardware.awgs.old_base import ProgramEntry
 
 try:
     import zhinst.utils
