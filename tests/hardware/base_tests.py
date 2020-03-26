4ca703253dfbe00f3fbc13fb910f9a62aeb891e7
inst2c <inst2c@gmail.com>
2020-03-26 17:21:06 +0100

    Fix bug in ProgramEntry and add test stubs

---
+++ tests/hardware/base_tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+from qupulse.hardware.awgs.base import ProgramEntry
+
+
+class ProgramEntryTests(unittest.TestCase):
+    def test_init(self):
+        raise NotImplementedError()
+
+    def test_sample_waveforms(self):
+        raise NotImplementedError()
