f41f9ef98b3aaf059df6b83ed3149ee14e1bb769
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2022-06-29 17:12:10 +0200

    Some constant pulse template generalizations

--- qupulse/pulses/constant_pulse_template.py
+++ qupulse/pulses/constant_pulse_template.py
@@ -10,6 +10,8 @@ import logging
 import numbers
 from typing import Any, Dict, List, Optional, Set, Union
 
+import cached_property
+
 from qupulse._program import ProgramBuilder
 from qupulse._program.waveforms import ConstantWaveform
 from qupulse.expressions import ExpressionScalar
@@ -20,6 +22,7 @@ from qupulse.pulses.pulse_template import (AtomicPulseTemplate, ChannelID,
                                            Loop, MeasurementDeclaration,
                                            PulseTemplate, Transformation,
                                            TransformingWaveform)
+from qupulse.serialization import PulseRegistryType
 
 __all__ = ["ConstantPulseTemplate"]
 
@@ -27,7 +30,9 @@ __all__ = ["ConstantPulseTemplate"]
 class ConstantPulseTemplate(AtomicPulseTemplate):  # type: ignore
 
     def __init__(self, duration: float, amplitude_dict: Dict[str, Any], identifier: Optional[str] = None,
-                 name: Optional[str] = None, measurements: Optional[List[MeasurementDeclaration]] = [], **kwargs: Any) -> None:
+                 name: Optional[str] = None, measurements: Optional[List[MeasurementDeclaration]] = (),
+                 registry: PulseRegistryType = None,
+                 **kwargs: Any) -> None:
         """ A qupulse waveform representing a multi-channel pulse with constant values
         
         Args:
@@ -39,11 +44,13 @@ class ConstantPulseTemplate(AtomicPulseTemplate):  # type: ignore
         super().__init__(identifier=identifier, measurements=measurements, **kwargs)
 
         self._duration = ExpressionScalar(duration)
-        self._amplitude_dict = amplitude_dict
+        self._amplitude_dict = {channel: ExpressionScalar(value) for channel, value in amplitude_dict.items()}
 
         if name is None:
             name = 'constant_pulse'
         self._name = name
+
+        self._register(registry=registry)
 
     def _as_expression(self):
         return self._amplitude_dict
@@ -56,7 +63,7 @@ class ConstantPulseTemplate(AtomicPulseTemplate):  # type: ignore
 
     def get_serialization_data(self) -> Any:
         data = super().get_serialization_data()
-        data.update({'name': self._name, 'duration': self._duration, '#amplitudes': self._amplitude_dict})
+        data.update({'name': self._name, 'duration': self._duration, 'amplitude_dict': self._amplitude_dict})
         return data
 
     @property
@@ -64,10 +71,13 @@ class ConstantPulseTemplate(AtomicPulseTemplate):  # type: ignore
         """Returns an expression giving the integral over the pulse."""
         return {c: self.duration * self._amplitude_dict[c] for c in self._amplitude_dict}
 
-    @property
+    @cached_property.cached_property
     def parameter_names(self) -> Set[str]:
         """The set of names of parameters required to instantiate this PulseTemplate."""
-        return set()
+        parameter_names = set(getattr(self._duration, 'variables', ()))
+        for value in self._amplitude_dict.values():
+            parameter_names.update(getattr(value, 'variables', ()))
+        return parameter_names
 
     @property
     def is_interruptable(self) -> bool:
@@ -78,7 +88,7 @@ class ConstantPulseTemplate(AtomicPulseTemplate):  # type: ignore
     @property
     def duration(self) -> ExpressionScalar:
         """An expression for the duration of this PulseTemplate."""
-        return (self._duration)
+        return self._duration
 
     @property
     def defined_channels(self) -> Set['ChannelID']:
@@ -99,9 +109,12 @@ class ConstantPulseTemplate(AtomicPulseTemplate):  # type: ignore
         for channel, value in self._amplitude_dict.items():
             mapped_channel = channel_mapping[channel]
             if mapped_channel is not None:
+                evaluator = getattr(value, 'evaluate_in_scope', None)
+                if evaluator:
+                    value = evaluator(parameters)
                 constant_values[mapped_channel] = value
 
         if constant_values:
-            return ConstantWaveform.from_mapping(self.duration, constant_values)
+            return ConstantWaveform.from_mapping(self.duration.evaluate_in_scope(parameters), constant_values)
         else:
             return None
