b8b8e0b1d9442fc4b7388182c48c36e1d1004849
Pieter Eendebak <pieter.eendebak@gmail.com>
2022-06-30 14:04:02 +0200

    add test

--- tests/pulses/method_pulse_tests.py
+++ tests/pulses/method_pulse_tests.py
@@ -6,10 +6,9 @@ from qupulse.expressions import ExpressionScalar
 from qupulse.pulses.plotting import render
 
 
-
 class MethodPulseTest(unittest.TestCase):
     def setUp(self) -> None:
-        pulse_method=lambda t: np.sin(.2*t)
+        def pulse_method(t): return np.sin(.2*t)
         self.fpt = MethodPulseTemplate(pulse_method, duration=100, channel='A')
 
 
@@ -31,18 +30,22 @@ class MethodPulsePropertyTest(MethodPulseTest):
         self.assertDictEqual(pulse.integral, {'default': 30})
         pulse = MethodPulseTemplate(pulse_method=lambda t: np.sin(t), duration=30)
         self.assertDictEqual(pulse.integral, {'default': ExpressionScalar(0.8457485501124153)})
-        
+
+    def test_get_serialization_data(self):
+        s = self.fpt.get_serialization_data()
+        self.assertEqual(s['channel'], 'A')
+        self.assertIsInstance(s['method'], str)
+
 
 class MethodPulseSequencingTest(MethodPulseTest):
     def test_build_waveform(self) -> None:
         wf = self.fpt.build_waveform({}, channel_mapping={'A': 'B'})
         self.assertEqual(wf.defined_channels, {'B'})
-        
+
     def test_sample(self) -> None:
-        times, values, _ = render(self.fpt.create_program(), sample_rate = 2)
-        np.testing.assert_almost_equal(values['A'], np.sin(.2 * np.arange(0,100.1, .5)))
+        times, values, _ = render(self.fpt.create_program(), sample_rate=2)
+        np.testing.assert_almost_equal(values['A'], np.sin(.2 * np.arange(0, 100.1, .5)))
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
-    
