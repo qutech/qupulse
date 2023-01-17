6329e4aa4f854f72fcfcf75780964b1a5a07c670
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2023-01-17 18:35:04 +0100

    Restructure expression code and start specifying a clean interface

---
+++ qupulse/expressions/__init__.py
@@ -0,0 +1,59 @@
+from typing import Type, TypeVar
+from numbers import Real
+
+import numpy as np
+import sympy
+
+from . import legacy, protocol
+
+
+__all__ = ["Expression", "ExpressionVector", "ExpressionScalar",
+           "NonNumericEvaluation", "ExpressionVariableMissingException"]
+
+
+Expression: Type[protocol.Expression] = legacy.Expression
+ExpressionScalar: Type[protocol.ExpressionScalar] = legacy.ExpressionScalar
+ExpressionVector: Type[protocol.ExpressionVector] = legacy.ExpressionVector
+
+
+ExpressionLike = TypeVar('ExpressionLike', str, Real, sympy.Expr, ExpressionScalar)
+
+
+class ExpressionVariableMissingException(Exception):
+    """An exception indicating that a variable value was not provided during expression evaluation.
+
+    See also:
+         qupulse.expressions.Expression
+    """
+
+    def __init__(self, variable: str, expression: Expression) -> None:
+        super().__init__()
+        self.variable = variable
+        self.expression = expression
+
+    def __str__(self) -> str:
+        return f"Could not evaluate <{self.expression}>: A value for variable <{self.variable}> is missing!"
+
+
+class NonNumericEvaluation(Exception):
+    """An exception that is raised if the result of evaluate_numeric is not a number.
+
+    See also:
+        qupulse.expressions.Expression.evaluate_numeric
+    """
+
+    def __init__(self, expression: Expression, non_numeric_result, call_arguments):
+        self.expression = expression
+        self.non_numeric_result = non_numeric_result
+        self.call_arguments = call_arguments
+
+    def __str__(self) -> str:
+        if isinstance(self.non_numeric_result, np.ndarray):
+            dtype = self.non_numeric_result.dtype
+
+            if dtype == np.dtype('O'):
+                dtypes = set(map(type, self.non_numeric_result.flat))
+                return f"The result of evaluate_numeric is an array with the types {dtypes} which is not purely numeric"
+        else:
+            dtype = type(self.non_numeric_result)
+        return f"The result of evaluate_numeric is of type {dtype} which is not a number"
