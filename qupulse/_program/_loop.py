ffa2e43739e842cde9def61bac05b8af43947d4d
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2023-08-21 08:44:24 +0200

    Add backwards compatibility link to new location

---
+++ qupulse/_program/_loop.py
@@ -0,0 +1,9 @@
+"""Backwards compatibility link to qupulse.program.loop"""
+
+from qupulse.program.loop import *
+
+import qupulse.program.loop
+
+__all__ = qupulse.program.loop.__all__
+
+del qupulse
