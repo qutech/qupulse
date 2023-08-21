40c298dbc7aaa47137d4a2a523be8b3aa61f6b1e
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2023-08-21 08:57:46 +0200

    Add link from old to new waveform location

---
+++ qupulse/_program/waveforms.py
@@ -0,0 +1,9 @@
+"""Backwards compatibility link to qupulse.program.waveforms"""
+
+from qupulse.program.waveforms import *
+
+import qupulse.program.waveforms
+
+__all__ = qupulse.program.waveforms.__all__
+
+del qupulse
