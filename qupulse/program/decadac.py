5a1cadf236919d723228254738b4dfc09415d1b7
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2023-09-01 12:49:51 +0200

    Add stub of deca dac program builder

---
+++ qupulse/program/decadac.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from typing import Mapping, Optional, Sequence, ContextManager, Iterable
+
+from qupulse import ChannelID, MeasurementWindow
+from qupulse.parameter_scope import Scope
+from qupulse.program import ProgramBuilder, HardwareTime, HardwareVoltage, Waveform, RepetitionCount, TimeType
+
+
+@dataclass
+class DecaDACASCIIProgram:
+    script: str
+    duration: TimeType
+
+
+class DecaDACASCIIBuilder:
+    def __init__(self):
+        pass
+
+    def inner_scope(self, scope: Scope) -> Scope:
+        """This function is necessary to inject program builder specific parameter implementations into the build
+        process."""
+        raise NotImplementedError('TODO')
+
+    def hold_voltage(self, duration: HardwareTime, voltages: Mapping[ChannelID, HardwareVoltage]):
+        raise NotImplementedError('TODO')
+
+    def play_arbitrary_waveform(self, waveform: Waveform):
+        raise NotImplementedError('Not implemented yet (postponed)')
+
+    def measure(self, measurements: Optional[Sequence[MeasurementWindow]]):
+        """Ignores measurements"""
+        pass
+
+    def with_repetition(self, repetition_count: RepetitionCount,
+                        measurements: Optional[Sequence[MeasurementWindow]] = None) -> Iterable['ProgramBuilder']:
+        raise NotImplementedError('TODO')
+
+    def with_sequence(self,
+                      measurements: Optional[Sequence[MeasurementWindow]] = None) -> ContextManager['ProgramBuilder']:
+        raise NotImplementedError('TODO')
+
+    def new_subprogram(self, global_transformation: 'Transformation' = None) -> ContextManager['ProgramBuilder']:
+        raise NotImplementedError('Not implemented yet (postponed)')
+
+    def with_iteration(self, index_name: str, rng: range,
+                       measurements: Optional[Sequence[MeasurementWindow]] = None) -> Iterable['ProgramBuilder']:
+        raise NotImplementedError('TODO')
+
+    def to_program(self) -> Optional[DecaDACASCIIProgram]:
+        """Further addition of new elements might fail after finalizing the program."""
+        raise NotImplementedError('TODO')
