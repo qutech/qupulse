ff646727dcda708239749914770714b5fe7b082c
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2025-01-04 16:53:54 +0100

    Remove dead code, improve readability and create dedicated vm.

--- qupulse/program/measurement.py
+++ qupulse/program/measurement.py
@@ -1,8 +1,9 @@
-from typing import Sequence, Mapping, Iterable, Optional, Union, ContextManager
+import contextlib
+from typing import Sequence, Mapping, Iterable, Optional, Union, ContextManager, Callable
 from dataclasses import dataclass
+from functools import cached_property
 
 import numpy
-from rich.measure import Measurement
 
 from qupulse.utils.types import TimeType
 from qupulse.program import (ProgramBuilder, Program, HardwareVoltage, HardwareTime,
@@ -10,142 +11,7 @@ from qupulse.program import (ProgramBuilder, Program, HardwareVoltage, HardwareT
 from qupulse.parameter_scope import Scope
 
 
-@dataclass
-class MeasurementNode:
-    windows: Sequence[MeasurementWindow]
-    duration: HardwareTime
-
-
-@dataclass
-class MeasurementRepetition(MeasurementNode):
-    body: MeasurementNode
-    count: RepetitionCount
-
-@dataclass
-class MeasurementSequence(MeasurementNode):
-    nodes: Sequence[tuple[HardwareTime, MeasurementNode]]
-
-
-@dataclass
-class MeasurementFrame:
-    commands: list['Command']
-    has_duration: bool
-
 MeasurementID = str | int
-
-
-class MeasurementBuilder(ProgramBuilder):
-    def __init__(self):
-        super().__init__()
-
-        self._frames = [MeasurementFrame([], False)]
-        self._ranges: list[tuple[str, range]] = []
-        self._repetitions = []
-        self._measurements = []
-        self._label_counter = 0
-
-    def _with_new_frame(self, measurements):
-        self._frames.append(MeasurementFrame([], False))
-        yield self
-        frame = self._frames.pop()
-        if not frame.has_duration:
-            return
-        parent = self._frames[-1]
-        parent.has_duration = True
-        if measurements:
-            parent.commands.extend(map(Measure, measurements))
-        return frame.commands
-
-    def inner_scope(self, scope: Scope) -> Scope:
-        """This function is necessary to inject program builder specific parameter implementations into the build
-        process."""
-        if self._ranges:
-            name, rng = self._ranges[-1]
-            return scope.overwrite({name: SimpleExpression(base=rng.start, offsets={name: rng.step})})
-        else:
-            return scope
-
-    def hold_voltage(self, duration: HardwareTime, voltages: Mapping[str, HardwareVoltage]):
-        """Supports dynamic i.e. for loop generated offsets and duration"""
-        self._frames[-1].commands.append(Wait(duration))
-        self._frames[-1].has_duration = True
-
-    def play_arbitrary_waveform(self, waveform: Waveform):
-        """"""
-        self._frames[-1].commands.append(Wait(waveform.duration))
-        self._frames[-1].has_duration = True
-
-    def measure(self, measurements: Optional[Sequence[MeasurementWindow]]):
-        """Unconditionally add given measurements relative to the current position."""
-        if measurements:
-            commands = self._frames[-1].commands
-            commands.extend(Measure(*meas) for meas in measurements)
-            self._frames[-1].has_duration = True
-
-    def with_repetition(self, repetition_count: RepetitionCount,
-                        measurements: Optional[Sequence[MeasurementWindow]] = None) -> Iterable['ProgramBuilder']:
-        """Measurements that are added to the new builder are dropped if the builder is empty upon exit"""
-        new_commands = yield from self._with_new_frame(measurements)
-        if new_commands is None:
-            return
-        parent = self._frames[-1]
-
-        self._label_counter += 1
-        label_idx = self._label_counter
-        parent.commands.append(LoopLabel(idx=label_idx, runtime_name=None, count=RepetitionCount))
-        parent.commands.extend(new_commands)
-        parent.commands.append(LoopJmp(idx=label_idx))
-
-    def with_sequence(self,
-                      measurements: Optional[Sequence[MeasurementWindow]] = None) -> ContextManager['ProgramBuilder']:
-        """
-
-        Measurements that are added in to the returned program builder are discarded if the sequence is empty on exit.
-
-        Args:
-            measurements: Measurements to attach to the potential child.
-        Returns:
-        """
-        new_commands = yield from self._with_new_frame(measurements)
-        if new_commands is None:
-            return
-        parent = self._frames[-1]
-        parent.commands.extend(new_commands)
-
-    def new_subprogram(self, global_transformation: 'Transformation' = None) -> ContextManager['ProgramBuilder']:
-        """Create a context managed program builder whose contents are translated into a single waveform upon exit if
-        it is not empty."""
-        yield self
-
-    def with_iteration(self, index_name: str, rng: range,
-                       measurements: Optional[Sequence[MeasurementWindow]] = None) -> Iterable['ProgramBuilder']:
-        self._ranges.append((index_name, rng))
-        new_commands = yield from self._with_new_frame(measurements)
-        self._ranges.pop()
-        if new_commands is None:
-            return
-        parent = self._frames[-1]
-
-        self._label_counter += 1
-        label_idx = self._label_counter
-        parent.commands.append(LoopLabel(idx=label_idx, runtime_name=index_name, count=len(rng)))
-        parent.commands.extend(new_commands)
-        parent.commands.append(LoopJmp(idx=label_idx))
-
-    def time_reversed(self) -> ContextManager['ProgramBuilder']:
-        self._frames.append(MeasurementFrame([], False))
-        yield self
-        frame = self._frames.pop()
-        if not frame.has_duration:
-            return
-
-        self._frames[-1].has_duration = True
-        self._frames[-1].commands.extend(_reversed_commands(frame.commands))
-
-    def to_program(self) -> Optional[Program]:
-        """Further addition of new elements might fail after finalizing the program."""
-        if self._frames[0].has_duration:
-            return self._frames[0].commands
 
 
 @dataclass
@@ -175,6 +41,145 @@ class LoopJmp:
 Command = Union[LoopLabel, LoopJmp, Wait, Measure]
 
 
+@dataclass
+class MeasurementInstructions(Program):
+    commands: Sequence[Command]
+
+    @cached_property
+    def duration(self) -> float:
+        latest = 0.
+
+        def process(_, begin, length):
+            nonlocal latest
+            end = begin + length
+            latest = max(latest, end)
+
+        vm = MeasurementVM(process)
+        vm.execute(commands=self.commands)
+        return latest
+
+
+@dataclass
+class MeasurementFrame:
+    commands: list['Command']
+    keep: bool
+
+
+class MeasurementBuilder(ProgramBuilder):
+    def __init__(self):
+        super().__init__()
+
+        self._frames = [MeasurementFrame([], False)]
+        self._ranges: list[tuple[str, range]] = []
+        self._repetitions = []
+        self._measurements = []
+        self._label_counter = 0
+
+    def _with_new_frame(self, measurements):
+        self._frames.append(MeasurementFrame([], False))
+        yield self
+        frame = self._frames.pop()
+        if not frame.keep:
+            return
+        self.measure(measurements)
+        # measure does not keep if there are no measurements
+        self._frames[-1].keep = True
+        return frame.commands
+
+    def inner_scope(self, scope: Scope) -> Scope:
+        """This function is necessary to inject program builder specific parameter implementations into the build
+        process."""
+        if self._ranges:
+            name, rng = self._ranges[-1]
+            return scope.overwrite({name: SimpleExpression(base=rng.start, offsets={name: rng.step})})
+        else:
+            return scope
+
+    def hold_voltage(self, duration: HardwareTime, voltages: Mapping[str, HardwareVoltage]):
+        """Supports dynamic i.e. for loop generated offsets and duration"""
+        self._frames[-1].commands.append(Wait(duration))
+        self._frames[-1].keep = True
+
+    def play_arbitrary_waveform(self, waveform: Waveform):
+        """"""
+        self._frames[-1].commands.append(Wait(waveform.duration))
+        self._frames[-1].keep = True
+
+    def measure(self, measurements: Optional[Sequence[MeasurementWindow]]):
+        """Unconditionally add given measurements relative to the current position."""
+        if measurements:
+            commands = self._frames[-1].commands
+            commands.extend(Measure(*meas) for meas in measurements)
+            self._frames[-1].keep = True
+
+    def with_repetition(self, repetition_count: RepetitionCount,
+                        measurements: Optional[Sequence[MeasurementWindow]] = None) -> Iterable['ProgramBuilder']:
+        """Measurements that are added to the new builder are dropped if the builder is empty upon exit"""
+        new_commands = yield from self._with_new_frame(measurements)
+        if new_commands is None:
+            return
+        parent = self._frames[-1]
+
+        self._label_counter += 1
+        label_idx = self._label_counter
+        parent.commands.append(LoopLabel(idx=label_idx, runtime_name=None, count=repetition_count))
+        parent.commands.extend(new_commands)
+        parent.commands.append(LoopJmp(idx=label_idx))
+
+    @contextlib.contextmanager
+    def with_sequence(self,
+                      measurements: Optional[Sequence[MeasurementWindow]] = None) -> ContextManager['ProgramBuilder']:
+        """
+
+        Measurements that are added in to the returned program builder are discarded if the sequence is empty on exit.
+
+        Args:
+            measurements: Measurements to attach to the potential child.
+        Returns:
+        """
+        new_commands = yield from self._with_new_frame(measurements)
+        if new_commands is None:
+            return
+        parent = self._frames[-1]
+        parent.commands.extend(new_commands)
+
+    @contextlib.contextmanager
+    def new_subprogram(self, global_transformation: 'Transformation' = None) -> ContextManager['ProgramBuilder']:
+        """Create a context managed program builder whose contents are translated into a single waveform upon exit if
+        it is not empty."""
+        yield self
+
+    def with_iteration(self, index_name: str, rng: range,
+                       measurements: Optional[Sequence[MeasurementWindow]] = None) -> Iterable['ProgramBuilder']:
+        self._ranges.append((index_name, rng))
+        new_commands = yield from self._with_new_frame(measurements)
+        self._ranges.pop()
+        if new_commands is None:
+            return
+        parent = self._frames[-1]
+
+        self._label_counter += 1
+        label_idx = self._label_counter
+        parent.commands.append(LoopLabel(idx=label_idx, runtime_name=index_name, count=len(rng)))
+        parent.commands.extend(new_commands)
+        parent.commands.append(LoopJmp(idx=label_idx))
+
+    def time_reversed(self) -> ContextManager['ProgramBuilder']:
+        self._frames.append(MeasurementFrame([], False))
+        yield self
+        frame = self._frames.pop()
+        if not frame.keep:
+            return
+
+        self._frames[-1].keep = True
+        self._frames[-1].commands.extend(_reversed_commands(frame.commands))
+
+    def to_program(self) -> Optional[Program]:
+        """Further addition of new elements might fail after finalizing the program."""
+        if self._frames[0].keep:
+            return MeasurementInstructions(self._frames[0].commands)
+
+
 def _reversed_commands(cmds: Sequence[Command]) -> Sequence[Command]:
     reversed_cmds = []
     jumps = {}
@@ -202,30 +207,26 @@ def _reversed_commands(cmds: Sequence[Command]) -> Sequence[Command]:
     return reversed_cmds
 
 
-def to_table(commands: Sequence[Command]) -> dict[str, numpy.ndarray]:
-    time = TimeType(0)
+class MeasurementVM:
+    """A VM that is capable of executing the measurement commands"""
 
-    memory = {}
-    counts = [None]
+    def __init__(self, callback: Callable[[str, float, float], None]):
+        self._time = TimeType(0)
+        self._memory = {}
+        self._counts = {}
+        self._callback = callback
 
-    tables = {}
-
-    def eval_hardware_time(t: HardwareTime):
+    def _eval_hardware_time(self, t: HardwareTime):
         if isinstance(t, SimpleExpression):
             value = t.base
             for (factor_name, factor_val) in t.offsets.items():
-                count = counts[memory[factor_name]]
+                count = self._counts[self._memory[factor_name]]
                 value += factor_val * count
             return value
         else:
             return t
 
-    def execute(sequence: Sequence[Command]) -> int:
-        nonlocal time
-        nonlocal tables
-        nonlocal memory
-        nonlocal counts
-
+    def _execute_after_label(self, sequence: Sequence[Command]) -> int:
         skip = 0
         for idx, cmd in enumerate(sequence):
             if idx < skip:
@@ -234,23 +235,30 @@ def to_table(commands: Sequence[Command]) -> dict[str, numpy.ndarray]:
                 return idx
             elif isinstance(cmd, LoopLabel):
                 if cmd.runtime_name:
-                    memory[cmd.runtime_name] = cmd.idx
-                if cmd.idx == len(counts):
-                    counts.append(0)
-                assert cmd.idx < len(counts)
+                    self._memory[cmd.runtime_name] = cmd.idx
 
                 for iter_val in range(cmd.count):
-                    counts[cmd.idx] = iter_val
-                    pos = execute(sequence[idx + 1:])
+                    self._counts[cmd.idx] = iter_val
+                    pos = self._execute_after_label(sequence[idx + 1:])
                 skip = idx + pos + 2
-            elif isinstance(cmd, Measure):
-                meas_time = float(eval_hardware_time(cmd.delay) + time)
-                meas_len = float(eval_hardware_time(cmd.length))
-                tables.setdefault(cmd.meas_id, []).append((meas_time, meas_len))
-            elif isinstance(cmd, Wait):
-                time += eval_hardware_time(cmd.duration)
 
-    execute(commands)
+            elif isinstance(cmd, Measure):
+                meas_time = float(self._eval_hardware_time(cmd.delay) + self._time)
+                meas_len = float(self._eval_hardware_time(cmd.length))
+                self._callback(cmd.meas_id, meas_time, meas_len)
+
+            elif isinstance(cmd, Wait):
+                self._time += self._eval_hardware_time(cmd.duration)
+
+    def execute(self, commands: Sequence[Command]):
+        self._execute_after_label(commands)
+
+
+def to_table(commands: Sequence[Command]) -> dict[str, numpy.ndarray]:
+    tables = {}
+
+    vm = MeasurementVM(lambda name, begin, length: tables.setdefault(name, []).append((begin, length)))
+    vm.execute(commands)
     return {
         name: numpy.array(values) for name, values in tables.items()
     }
