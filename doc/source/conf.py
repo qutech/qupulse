04e0e1dd1d7b809086a4ffd2eebce1f12b2d0617
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2025-07-07 15:18:13 +0200

    Do not include extra class __init__

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -63,7 +63,7 @@ autosummary_mock_imports = autodoc_mock_imports = ['atsaverage', 'zhinst', 'teaw
 
 autoclass_content = 'both'
 autosummary_generate = True
-napoleon_include_init_with_doc = True
+napoleon_include_init_with_doc = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
