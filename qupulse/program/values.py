c0bd6e0058f5072455ed3c6bf57f843e7e0143f6
Nomos11 <82180697+Nomos11@users.noreply.github.com>
2025-07-28 18:36:02 +0200

    RDP

--- qupulse/program/values.py
+++ qupulse/program/values.py
@@ -2,12 +2,13 @@
 
 from dataclasses import dataclass
 from numbers import Real
-from typing import TypeVar, Generic, Mapping, Union, List
+from typing import TypeVar, Generic, Mapping, Union, List, Tuple, Optional
 from types import NotImplementedType
+
+import numpy as np
 
 from qupulse.program.volatile import VolatileRepetitionCount
 from qupulse.utils.types import TimeType
-
 from qupulse.expressions import sympy as sym_expr
 from qupulse.utils.sympy import _lambdify_modules
 
@@ -140,7 +141,10 @@ class DynamicLinearValue(Generic[NumVal]):
     def __truediv__(self, other):
         inv = 1 / other
         return self.__mul__(inv)
-
+    
+    def __hash__(self):
+        return hash((self.base,frozenset(sorted(self.factors.items()))))
+    
     @property
     def free_symbols(self):
         """This is required for the :py:class:`sympy.expr.Expr` interface compliance. Since the keys of
@@ -167,6 +171,95 @@ class DynamicLinearValue(Generic[NumVal]):
             self
         """
         return self
+
+
+# is there any way to cast the numpy cumprod to int?
+int_type = Union[np.int64,np.int32,int]
+
+class ResolutionDependentValue(Generic[NumVal]):
+    """This is a potential runtime-evaluable expression of the form
+    
+    o + sum_i  b_i*m_i
+    
+    with (potential) float o, b_i and integers m_i. o and b_i are rounded(gridded)
+    to a resolution given upon __call__.
+    
+    The main use case is the correct rounding of increments used in command-based
+    voltage scans on some hardware devices, where an imprecise numeric value is
+    looped over m_i times and could, if not rounded, accumulate a proportional
+    error leading to unintended drift in output voltages when jump-back commands
+    afterwards do not account for the deviations.
+    Rounding the value preemptively and supplying corrected values to jump-back
+    commands prevents this.
+    """
+    def __init__(self,
+                 bases: Tuple[NumVal],
+                 multiplicities: Tuple[int],
+                 offset: NumVal):
+
+        self.bases = tuple(bases)
+        self.multiplicities = tuple(multiplicities)
+        self.offset = offset
+        self.__is_time_or_int = all(isinstance(b,(TimeType,int_type)) for b in bases) and isinstance(offset,(TimeType,int_type))
+
+    #this is not to circumvent float errors in python, but rounding errors from awg-increment commands.
+    #python float are thereby accurate enough
+    def __call__(self, resolution: Optional[float]) -> Union[NumVal,TimeType]:
+        #with resolution = None handle TimeType/int case?
+        if resolution is None:
+            assert self.__is_time_or_int
+            return sum(b*m for b,m in zip(self.bases,self.multiplicities)) + self.offset
+        #resolution as float value of granularity of base val.
+        #to avoid conflicts between positive and negative vals from casting
+        #half to even, use abs val
+        return sum(np.sign(b) * round(abs(b) / resolution) * m * resolution for b,m in zip(self.bases,self.multiplicities))\
+             + np.sign(self.offset) * round(abs(self.offset) / resolution) * resolution
+
+    def __bool__(self):
+        #if any value is not zero - this helps for some checks
+        return any(bool(b) for b in self.bases) or bool(self.offset)
+
+    def __add__(self, other):
+        # this should happen in the context of an offset being added to it, not the bases being modified.
+        if isinstance(other, (float, int, TimeType)):
+            return ResolutionDependentValue(self.bases,self.multiplicities,self.offset+other)
+        return NotImplemented
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        return self.__add__(-other)
+
+    def __mul__(self, other):
+        # this should happen when the amplitude is being scaled
+        # multiplicities are not affected
+        if isinstance(other, (float, int, TimeType)):
+            return ResolutionDependentValue(tuple(b*other for b in self.bases),self.multiplicities,self.offset*other)
+        return NotImplemented
+
+    def __rmul__(self,other):
+        return self.__mul__(other)
+
+    def __truediv__(self,other):
+        return self.__mul__(1/other)
+
+    def __float__(self):
+        return float(self(resolution=None))
+
+    def __str__(self):
+        return f"RDP of {sum(b*m for b,m in zip(self.bases,self.multiplicities)) + self.offset}"
+
+    def __repr__(self):
+        return "RDP("+",".join([f"{k}="+v.__str__() for k,v in vars(self).items()])+")"
+
+    def __eq__(self,o):
+        if not isinstance(o,ResolutionDependentValue):
+            return False
+        return self.__dict__ == o.__dict__
+
+    def __hash__(self):
+        return hash((self.bases,self.offset,self.multiplicities,self.__is_time_or_int))
 
 
 # TODO: hackedy, hackedy
--- tests/program/values_tests.py
+++ tests/program/values_tests.py
@@ -8,7 +8,7 @@ from qupulse.pulses import *
 from qupulse.program.linspace import *
 from qupulse.program.transformation import *
 from qupulse.pulses.function_pulse_template import FunctionPulseTemplate
-from qupulse.program.values import DynamicLinearValue
+from qupulse.program.values import DynamicLinearValue, ResolutionDependentValue
 from qupulse.utils.types import TimeType
 
 
@@ -59,7 +59,7 @@ class DynamicLinearValueTests(TestCase):
         self.assertEqual(self.d==1,False)
         self.assertEqual(self.d==1+1j,False)
         # self.assertEqual(self.d>-101,True) #if one wants to allow these comparisons
-        self.assertEqual(self.d<TimeType(24/5),True)
+        # self.assertEqual(self.d<TimeType(24/5),True)  #if one wants to allow these comparisons
         
         self.assertEqual(self.d==self.d,True)
         self.assertEqual(self.d+1==self.d,False)
@@ -74,3 +74,50 @@ class DynamicLinearValueTests(TestCase):
     def test_sympy(self):
         self.assertEqual(self.d._sympy_(), self.d)
         self.assertEqual(self.d.replace(1,1), self.d)
+        
+    def test_hash(self):
+        self.assertEqual(hash(self.d), hash(self.d))
+        self.assertNotEqual(hash(self.d), hash(self.d3))
+        
+        
+
+class ResolutionDependentValueTests(TestCase):
+    def setUp(self):
+        self.d = ResolutionDependentValue((np.pi*1e-5,np.e*1e-5),(10,20),0.1)
+        self.d2 = ResolutionDependentValue((np.e*1e-5,np.pi*1e-5),(10,20),-0.1)
+        self.dtt = ResolutionDependentValue((TimeType(12,5),TimeType(14,5)),(10,20),TimeType(12,5))
+        self.dint = ResolutionDependentValue((1,2),(10,20),1)
+        
+        self._default_res = 2**-16
+        
+    def test_call(self):
+        val = self.d(self._default_res)
+        #expected to round to 2*res each, so 60*2**16, offset also rounded
+        expected_val = 2**-16 * 60 + 0.100006103515625
+        self.assertAlmostEqual(val,expected_val,places=12)
+        self.assertEqual((val/self._default_res)%1,0)
+        
+        #if no resolution must be tt or int
+        self.assertEqual(self.dtt(None),TimeType(412, 5))
+        self.assertEqual(self.dint(None),51)
+        
+    def test_dunder(self):
+        self.assertEqual(bool(self.d), True)
+        self.assertRaises(TypeError, lambda: self.d+self.d2)
+        self.assertEqual(self.d-0.1,
+                         ResolutionDependentValue((np.pi*1e-5,np.e*1e-5),(10,20),0.0))
+        self.assertEqual(self.d*2,
+                         ResolutionDependentValue((np.pi*1e-5*2,np.e*1e-5*2),(10,20),0.2))
+        self.assertEqual(self.d/2,
+                         ResolutionDependentValue((np.pi*1e-5/2,np.e*1e-5/2),(10,20),0.1/2)) 
+        
+        self.assertRaises(AssertionError, lambda: float(self.d))
+        
+        try: str(self.d)
+        except: self.fail()
+        
+        self.assertEqual(hash(self.d), hash(self.d))
+        self.assertNotEqual(hash(self.d), hash(self.d2))
+        
+        self.assertEqual(self.d==ResolutionDependentValue((np.pi*1e-5,np.e*1e-5),(10,20),0.1),True)
+        
